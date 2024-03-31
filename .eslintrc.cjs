ea8af2c8f42828889afc6c8b1bbd5c43ca06cff4
Volkov <volkov.aliksandr0203@gmail.com>
2024-04-01 01:54:38 +0300

    Changed config for .eslintrc.cjs

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,17 +2,20 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended-type-checked",
+    "plugin:react-hooks/recommended",
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  parserOptions: { project: ["./tsconfig.json", "./tsconfig.node.json"] },
+  plugins: ["@typescript-eslint", "react-refresh"],
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
+  parser: "@typescript-eslint/parser",
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
+    "react-refresh/only-export-components": [
+      "warn",
       { allowConstantExport: true },
     ],
+    // "no-multi-spaces": "error",
+    // "no-trailing-spaces": "error"
   },
-}
+};
